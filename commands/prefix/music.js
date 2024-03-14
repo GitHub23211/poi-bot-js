90ed7270beee9cebf80a0c6fc332781db7f06a1f
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-03-14 20:40:22 +1100

    implemented basic commands for joining and leaving vc

--- commands/prefix/music.js
+++ commands/prefix/music.js
@@ -1,14 +1,31 @@
 const PrefixCommandBuilder = require('../../components/prefix-command-builder.js')
+const { getVoiceConnection, joinVoiceChannel } = require('@discordjs/voice')
 
 const play = {
     data: new PrefixCommandBuilder()
-            .setName('test')
-            .setDescription('test'),
-    async execute(interaction) {
-        await interaction.reply('test')
+            .setName('play')
+            .setDescription('Play music from a YouTube link'),
+    async execute(message) {
+        const channel = message.member.voice.channel
+        joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+        message.reply("yes")
+    }
+}
+
+const stop = {
+    data: new PrefixCommandBuilder()
+            .setName('stop')
+            .setDescription('Disconnects bot from voice channel'),
+    async execute(message) {
+        getVoiceConnection(message.member.voice.channel.guild.id).destroy()
     }
 }
     
 module.exports = {
-    play
+    play,
+    stop
 }
