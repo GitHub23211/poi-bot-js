f6a5af19a7ebd2915783bfe24a44bbb067e4a692
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-03-14 19:27:50 +1100

    prefix command event handler

--- events/commands.js
+++ events/commands.js
@@ -1,5 +1,9 @@
 const { Events } = require('discord.js')
 
+require('dotenv').config()
+
+const PREFIX = 'f'
+
 const handleSlash = {
     name: Events.InteractionCreate,
     async execute(interaction) {
@@ -18,6 +22,20 @@ const handleSlash = {
     }
 }
 
+const handlePrefix = {
+    name: Events.MessageCreate,
+    async execute(message) {
+        if(!message.guildId == process.env.GUILD_ID || !message.content.startsWith(PREFIX)) return
+
+        const command = message.client.prefixes.get(message.content.slice(2, message.content.length))
+
+        if(!command) return
+
+        console.log(command)
+    }
+}
+
 module.exports = {
-    handleSlash
+    handleSlash,
+    handlePrefix
 }
