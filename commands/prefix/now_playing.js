c9860d04c6e94f8ba4ba30af81003d65dfca2b44
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-03-25 16:22:51 +1100

    command to show currently playing song

---
+++ commands/prefix/now_playing.js
@@ -0,0 +1,18 @@
+const PrefixCommandBuilder = require('../../components/prefix-command-builder.js')
+
+/**
+ * Messages currently playing song
+ * @param message - Message passed from Event
+ * @param options - Empty array; not used.
+ */
+const np = {
+    data: new PrefixCommandBuilder()
+            .setName('np')
+            .setDescription('Messages currently playing song'),
+    async execute(message, options) {
+        if(!message.client.song) { await message.reply('No song currently playing.'); return}
+        await message.channel.send(`Currently playing: ${message.client.song[0]}`)
+    }
+}
+    
+module.exports = { np }
